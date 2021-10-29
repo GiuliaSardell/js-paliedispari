e1e23ef8cf5cb2e03e108d30271c37cf9e262b87
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-10-29 15:30:16 +0200

    function parola

--- js/script.js
+++ js/script.js
@@ -3,21 +3,42 @@
 // se n = n al contrario = stampa la parola
 
 
+// let parola = prompt('inserire una parola');
+// console.log('la parola scelta è: ',parola)
+
+// let parolaDivisa = parola.split('');
+// console.log('creo un array con le lettere della parola: ',parolaDivisa);
+
+
+// let arreyContrario = parolaDivisa.reverse()
+// console.log('Ribalto l ordine delle lettere: ',arreyContrario)
+
+// let parolaInvertita = arreyContrario.join('')
+// console.log('Trasformo il nuovo array invertito in una stringa: ',parolaInvertita)
+
+// if(parola === parolaInvertita){
+//   console.log('La parola è palindroma')
+// }else {
+//   console.log('La parola non è palindroma')
+// }
+
+
 let parola = prompt('inserire una parola');
-console.log('la parola scelta è: ',parola)
 
-let parolaDivisa = parola.split('');
-console.log('creo un array con le lettere della parola: ',parolaDivisa);
+let parolaInversa = invertiParola(parola);
 
-
-let arreyContrario = parolaDivisa.reverse()
-console.log('Ribalto l ordine delle lettere: ',arreyContrario)
-
-let parolaInvertita = arreyContrario.join('')
-console.log('Trasformo il nuovo array invertito in una stringa: ',parolaInvertita)
-
-if(parola === parolaInvertita){
+if(parola === parolaInversa){
   console.log('La parola è palindroma')
 }else {
   console.log('La parola non è palindroma')
+}
+
+
+function invertiParola(parola){
+  let parolaInversa = parola.split('').reverse().join('');  
+
+  console.log(parola)
+  console.log(parolaInversa)
+  return parolaInversa;
+
 }
