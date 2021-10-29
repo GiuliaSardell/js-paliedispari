a19da2263bab7729f8893b8538565f3a052b17d8
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-10-29 15:25:24 +0200

    parola palindroma

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// prompt inserire una parola = n
+// creare una funzione che riconosca n al contrario
+// se n = n al contrario = stampa la parola
+
+
+let parola = prompt('inserire una parola');
+console.log('la parola scelta è: ',parola)
+
+let parolaDivisa = parola.split('');
+console.log('creo un array con le lettere della parola: ',parolaDivisa);
+
+
+let arreyContrario = parolaDivisa.reverse()
+console.log('Ribalto l ordine delle lettere: ',arreyContrario)
+
+let parolaInvertita = arreyContrario.join('')
+console.log('Trasformo il nuovo array invertito in una stringa: ',parolaInvertita)
+
+if(parola === parolaInvertita){
+  console.log('La parola è palindroma')
+}else {
+  console.log('La parola non è palindroma')
+}
